76e9e488deb5068f59d740f762cccb676ecdfdd4
Armando Ortiz <armandopenafiel12@gmail.com>
2021-06-19 21:36:02 -0500

    Faltó una coma...

--- setup.py
+++ setup.py
@@ -11,14 +11,14 @@ setup(
   download_url = 'https://github.com/JAOP1/pyristic/archive/refs/tags/0.1.tar.gz',\
   keywords = ['Optimization', 'Metaheuristic', 'Python'],\
   install_requires=[
-          'numpy',
-          'tqdm',
-          'numba'
+          'numpy>=1.15.4',
+          'tqdm>=4.28.1',
+          'numba>=0.41.0'
       ],\
   classifiers=[
     'Development Status :: 3 - Alpha',
     'Intended Audience :: Developers',
-    'Intended Audience :: Science/Research'
+    'Intended Audience :: Science/Research',
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 3.7'
